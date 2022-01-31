a7d0baf05a430967765895c86a3ccc5bb94dcf4e
ayoub <gholami.ayub73@gmail.com>
2022-01-31 21:04:17 +0330

    changing name.

--- free_translations/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('martor/', include('martor.urls')),
-    path('user/', include('profile.urls')),
-]
