3c0800f5b73006005e17da0ee7b7bf28cdc33cf4
ayoub <gholami.ayub73@gmail.com>
2022-01-31 21:01:51 +0330

    name of the project changed.

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('martor/', include('martor.urls')),
+    path('user/', include('user_account.urls')),
+    path('', include('translator.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
